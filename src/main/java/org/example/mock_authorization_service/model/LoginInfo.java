e6a336e2e6c0e53ba14a55d5aeddf9530c71b6ed
Luda <violentvioletlucy@yandex.ru>
2025-09-11 15:34:46 +0300

    Добавила lombok

--- src/main/java/org/example/mock_authorization_service/model/LoginInfo.java
+++ src/main/java/org/example/mock_authorization_service/model/LoginInfo.java
@@ -1,7 +1,11 @@
 package org.example.mock_authorization_service.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginInfo {
 
     @NotBlank(message = "Login must not be blank")
@@ -15,29 +19,5 @@ public class LoginInfo {
     public LoginInfo(String password, String login) {
         this.password = password;
         this.login = login;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
     }
 }
